54080ae89219fcf852c6194a10f2ce7e49b6477f
aluka0518 <warabiandmatchaandkinako@gmail.com>
2026-08-19 15:22:49 +0900

    feat: 回帰画面を追加

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import home
+from .views import again,home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', home, name='home'),
+    
+    path('again/', again, name='again'),
 
     # Qhymiaアカウント
     path('', include('apps.accounts.urls')),
