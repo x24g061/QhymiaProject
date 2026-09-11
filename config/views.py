fd0b122713cede27f4419c7894de76a50b31c0b6
aluka0518 <warabiandmatchaandkinako@gmail.com>
2026-09-11 10:55:10 +0900

    認証+sns画面

--- config/views.py
+++ config/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def home(request):
     return render(request, 'home.html')
 
 
+@login_required
 def again(request):
     return render(request, 'again.html')
