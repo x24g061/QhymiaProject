86f781ae49ff0b3928211ca7f2946e9c351b6dbd
aluka0518 <warabiandmatchaandkinako@gmail.com>
2026-09-14 10:50:26 +0900

    feat: SNSのお気に入りと非同期処理を改善

--- apps/sns/views.py
+++ apps/sns/views.py
@@ -2,15 +2,28 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
-from .models import Like, Post
-
+from .models import Favorite, Like, Post
+from django.http import JsonResponse
 
 @login_required
 def sns_page(request):
+    view_mode = request.GET.get("view", "all")
+
     posts = Post.objects.select_related(
         "user",
         "user__character"
     ).order_by("-created_at")
+
+    if view_mode == "favorites":
+        posts = posts.filter(
+            favorites__user=request.user
+        )
+
+    favorite_post_ids = set(
+        Favorite.objects.filter(
+            user=request.user
+        ).values_list("post_id", flat=True)
+    )
 
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
@@ -21,7 +34,6 @@ def sns_page(request):
             post.save()
 
             return redirect("sns")
-
     else:
         form = PostForm()
 
@@ -31,6 +43,8 @@ def sns_page(request):
         {
             "posts": posts,
             "form": form,
+            "favorite_post_ids": favorite_post_ids,
+            "view_mode": view_mode,
         },
     )
 
@@ -44,14 +58,43 @@ def toggle_like(request, post_id):
         post=post,
     ).first()
 
+    liked = False
+
     if like:
-        # すでにいいねしていたら解除
         like.delete()
     else:
-        # まだいいねしていなければ追加
         Like.objects.create(
             user=request.user,
             post=post,
         )
+        liked = True
 
-    return redirect("sns")
+    return JsonResponse({
+        "liked": liked,
+        "like_count": post.likes.count(),
+    })
+
+
+@login_required
+def toggle_favorite(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    favorite = Favorite.objects.filter(
+        user=request.user,
+        post=post,
+    ).first()
+
+    favorited = False
+
+    if favorite:
+        favorite.delete()
+    else:
+        Favorite.objects.create(
+            user=request.user,
+            post=post,
+        )
+        favorited = True
+
+    return JsonResponse({
+        "favorited": favorited,
+    })
--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -25,3 +27,9 @@ urlpatterns = [
     #SHOP画面
     path("shop/", include("apps.shop.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
