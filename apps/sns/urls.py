ab9cdf61aa40953ffab3c840eb507035f44c2534
aluka0518 <warabiandmatchaandkinako@gmail.com>
2026-09-15 10:12:22 +0900

    feat: SNS投稿削除機能を追加

--- apps/sns/urls.py
+++ apps/sns/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from .views import sns_page, toggle_favorite, toggle_like
-
+from .views import delete_post, sns_page, toggle_favorite, toggle_like
 
 urlpatterns = [
     path("", sns_page, name="sns"),
     path("like/<int:post_id>/", toggle_like, name="toggle_like"),
+    path(
+        "delete/<int:post_id>/",
+        delete_post,
+        name="delete_post",
+    ),
     path(
         "favorite/<int:post_id>/",
         toggle_favorite,
