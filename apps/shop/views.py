174dd4ccab8a728ee1c5a2bec2f09c9abefb7e54
x24g061 <x24g061@mitsuhashi.ac.jp>
2026-09-16 09:59:28 +0900

    feat: ゲーム画面をログイン必須にする

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import ShopStock
 
 
+@login_required
 def shop(request):
     shop_stocks = (
         ShopStock.objects
